361a33b36c0ad82b2f8c24978207d7ae1e473faf
Christina Lambert <clambert0615@gmail.com>
2020-07-15 17:31:11 -0400

    added CRUD functions

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,21 +8,30 @@ import {MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
+
   heroes: Hero[];
   constructor(private heroService: HeroService, private messageService: MessageService) { }
   ngOnInit() {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
        .subscribe(heroes => this.heroes = heroes);
   }
-
+  // Add a hero
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => { this.heroes.push(hero);
+    });
+  }
+  // Delete a hero
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !==hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
 
 }
